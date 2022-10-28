134870ef6a405feb7c604e92f5c7b6ad6dcdb90a
Muhammad Rayhan Messala Ahsan <rayhanahsan14@gmail.com>
2022-10-28 10:10:47 +0700

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.lightBlue,
       ),
-      home: NavigatorScreen(),
+      home: SplashScreen(),
     );
   }
 }
