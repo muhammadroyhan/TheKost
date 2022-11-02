9f0bde0342abb7a9f4c2e38cf7cf5f042e39d841
Muhammad Rayhan Messala Ahsan <rayhanahsan14@gmail.com>
2022-11-02 19:17:56 +0700

    two commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:the_kost/pages/Navigator_Screen.dart';
-import 'package:the_kost/pages/home_screen.dart';
 import 'package:the_kost/pages/login_screen.dart';
-import 'package:the_kost/pages/profil_screen.dart';
 import 'package:the_kost/pages/splash_screen.dart';
 
 void main() async {
